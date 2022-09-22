cee00513f4b0d5bdd8658544b96adad2ce82be22
nguyenvudangkhoa189 <nguyenvudangkhoa189@gmail.com>
2022-09-22 20:48:10 +0700

    let's the users input and print the number

---
+++ example2.cpp
@@ -0,0 +1,16 @@
+#include <format>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Please enter a number (number > 0): " << endl;
+    cin >> number;
+
+    cout << "The number is: " << number << endl;
+    return 0;
+}
+
+
